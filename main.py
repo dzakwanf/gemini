09855b5b3e8c3b317523fcbea8271f094b08d000
M Dzakwan Falih <82427931+dzakwanf@users.noreply.github.com>
2024-01-18 13:47:14 +0000

    update

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ import requests
 import os
 
 
+# Add custom CSS to hide the GitHub icon
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 st.set_page_config(
     page_title="Dzakwan Chat",
@@ -14,7 +21,7 @@ st.set_page_config(
     layout="wide",
 )
 # Path: Main.py
-#Author: Sergio Demis Lopez Martinez
+
 #------------------------------------------------------------
 #HEADER
 st.markdown('''
